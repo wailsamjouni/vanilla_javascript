a847fb996678dc9dd55be463d50b4ddd9ce65166
wailsamjouni <waill.samjouni@gmail.com>
2022-04-08 18:35:50 +0200

    homepage finished

--- wtube/script.js
+++ wtube/script.js
@@ -1,5 +1,7 @@
 const menuIcon = document.querySelector('.nav__menuicon');
 const sidebar = document.querySelector('.sidebar');
+const bodyContainer = document.querySelector('.body');
 menuIcon.addEventListener('click', () => {
     sidebar.classList.toggle('collapse');
+    bodyContainer.classList.toggle('fitbody')
 })
