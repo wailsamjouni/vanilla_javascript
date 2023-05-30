92819f0d3d61173332a6f9d3dcf4938df4ee686b
wailsamjouni <waill.samjouni@gmail.com>
2023-05-30 20:18:31 +0200

    Tic-tac-toe

---
+++ Tic-tac-toe/script.js
@@ -0,0 +1,69 @@
+const gameBoard = document.querySelector("#gameboard");
+const infoDisplay = document.querySelector("#info");
+
+const startCells = ["", "", "", "", "", "", "", "", ""];
+
+let go = "circle";
+infoDisplay.textContent = "Circle goes first";
+
+const checkScore = () => {
+  const allSquares = document.querySelectorAll(".square");
+  console.log(allSquares[4]);
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ];
+
+  winningCombos.forEach((array) => {
+    const circleWins = array.every((cell) =>
+      allSquares[cell].firstChild?.classList.contains("circle")
+    );
+
+    if (circleWins) {
+      infoDisplay.textContent = "Circle Wins!";
+      allSquares.forEach((square) =>
+        square.replaceWith(square.cloneNode(true))
+      );
+      return;
+    }
+  });
+
+  winningCombos.forEach((array) => {
+    const crossWins = array.every((cell) => {
+      allSquares[cell].firstChild?.classList.contains("cross");
+    });
+    if (crossWins) {
+      infoDisplay.textContent = "Cross Wins!";
+      allSquares.forEach((square) =>
+        square.replaceWith(square.cloneNode(true))
+      );
+      return;
+    }
+  });
+};
+
+const addGo = (event) => {
+  const goDisplay = document.createElement("div");
+  goDisplay.classList.add(go);
+  event.target.append(goDisplay);
+  go = go === "circle" ? "cross" : "circle";
+  infoDisplay.textContent = "it is now " + go + "'s go.";
+  event.target.removeEventListener("click", addGo);
+  checkScore();
+};
+
+const createBoard = () => {
+  startCells.forEach((_cell, index) => {
+    const cellElement = document.createElement("div");
+    cellElement.classList.add("square");
+    cellElement.id = index;
+    cellElement.addEventListener("click", addGo);
+    gameBoard.append(cellElement);
+  });
+};
+
+createBoard();
